f766d2cbeda56cddb5c4832e2d531d7748c77026
dni ⚡ <office@dnilabs.com>
2024-11-29 09:07:07 +0100

    fix: timestamp on api response should be int not date (#39)
    * fix: timestamp on api response should be int not date
    
    closes #38
    
    * fix: expiry time

--- views_api.py
+++ views_api.py
@@ -106,7 +106,7 @@ async def lndhub_gettxs(
             "type": "paid_invoice",
             "fee": payment.fee / 1000,
             "value": int(payment.amount / 1000),
-            "timestamp": payment.time,
+            "timestamp": int(payment.time.timestamp()),
             "memo": (
                 payment.extra and payment.extra.get("comment") or payment.memo
                 if not payment.pending
@@ -144,8 +144,12 @@ async def lndhub_getuserinvoices(
             "payment_hash": payment.payment_hash,
             "ispaid": payment.success,
             "amt": int(payment.amount / 1000),
-            "expire_time": int(time.time() + 1800),
-            "timestamp": payment.time,
+            "expire_time": (
+                int(payment.expiry.timestamp())
+                if payment.expiry
+                else payment.time.timestamp() + 3600
+            ),
+            "timestamp": int(payment.time.timestamp()),
             "type": "user_invoice",
         }
         for payment in reversed(
